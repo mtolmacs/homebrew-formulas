82010e0422b1f785e81d71b76d121b80569cdc5b
Mark Tolmacs <mark@lazycat.hu>
2025-09-06 14:06:23 +0200

    Add jj-fzf

---
+++ jj-fzf.rb
@@ -0,0 +1,18 @@
+class HermesEngine < Formula
+  desc "FZF-based TUI for JJ VCS"
+  homepage "https://github.com/tim-janik/jj-fzf"
+  url "https://github.com/tim-janik/jj-fzf/releases/download/v0.32.0/jj-fzf-0.32.0.tar.zst"
+  sha256 "73acac52b523376bfe580a7b9f00245856cb8da0d23f104dfd8dfab56792d202"
+  license "MPL 2.0"
+
+  # depends_on "cmake" => :build
+  # depends_on "ninja" => :build
+
+  def install
+    system "make", "all", "install", "DESTDIR=#{prefix}"    
+  end
+
+  test do
+    system bin, "jj-fzf", "--version"
+  end
+end
